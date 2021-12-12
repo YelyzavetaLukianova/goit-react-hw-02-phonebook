59dd4e01947d86e416463bc54c66b35c425c967d
YelyzavetaLukianova <yelyzaveta.lukianova@gmail.com>
2021-12-12 21:50:07 +0300

    hw2secondtask

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+import Phonebook from './components/Phonebook/Phonebook';
+// import Statistics from './components/Statistics';
+// import FriendList from './components/FriendList';
+// import TransactionHistory from './components/TransactionHistory';
+// import data from './data/data';
+// import transactions from './data/transactions';
+
+function App() {
+  return <Phonebook />;
+}
+
+export default App;
